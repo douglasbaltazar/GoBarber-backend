bbf35c83f3e8be239eed88c344f5f69e04f6b4d6
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-05-12 19:03:32 -0300

    Atualizar avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
-import CreateAppointmentService from '../services/CreateAppointmentService'
 import CreateUserService from '../services/CreateUserService'
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+
 const usersRouter = Router()
+const upload = multer(uploadConfig)
+
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -26,6 +31,20 @@ usersRouter.post('/', async (request, response) => {
     } catch(err) {
         return response.status(400).json({ error: err.message })
     }
+})
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async(request, response) => {
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename
+        })
+        return response.json(user)
+    } catch(err) {
+        return response.status(400).json({ error: err.message })
+    }
+    
 })
 
 export default usersRouter;
